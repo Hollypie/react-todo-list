40f1eb61e0a4717dd5e4d084b0cdb226639b81f5
Hollypie <hbriggs@byupathway.edu>
2025-11-03 17:10:10 -0700

    fixed local storage issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,36 @@ export default function App() {
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS")
     if (localValue == null) return []
-
     return JSON.parse(localValue)
-  }) 
+  })
   
   useEffect(() => {
-    localStorage.setItem("ITEM", JSON.stringify(todos))
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
   }, [todos])
 
   function addTodo(title) {
-    setTodos(currentTodos => {
-          return [
-              ...currentTodos,
-              { id: crypto.randomUUID(), title, completed: false },
-          ]
-      })
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false },
+    ])
   }
 
 
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = completed
-          return { ...todo, completed }
-        }
-
-        return todo
-      })
-    })
+    function toggleTodo(id, completed) {
+    setTodos(currentTodos =>
+      currentTodos.map(todo =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    )
   }
 
   function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
+    setTodos(currentTodos => currentTodos.filter(todo => todo.id !== id))
   }
 
   return (
-  <>
-  <NewTodoForm onSubmit={addTodo} />
+    <>
+      <NewTodoForm onSubmit={addTodo} />
       <h1 className="header">Todo List</h1>
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
